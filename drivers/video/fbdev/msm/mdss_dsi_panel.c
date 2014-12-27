523a324e8a7d13d5bcbcd04c721ec6eb2c0d67f7
yank555-lu <yank555.lu@gmail.com>
2014-12-27 04:03:16 +0100

    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend
    Version v1.7.1 (faux123/Yank555.lu)
    
    powersuspend: new PM kernel driver for Android w/o early_suspend
    
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a replacement
    for it and existing early_suspend drivers can be easily adapted to use this
    new replacement driver
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    powersuspend: fix logci derps :p
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: remove userspace dependency from powersuspend
    
    make powersuspend not depend on a userspace initiator anymore, but use a hook
    in autosleep instead.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: add back userpace control w/ default kernel control
    
    make kernel / userspace mode switchable
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: default to userspace for now
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
    
    - add an alternative hook for screen on / off detection in the display
      pannel driver
    - sleeps  ~0.1s sooner, wakes up ~1s later then autosleep
    - clean up source formatting a bit (Paul Reioux)
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    mdss_dsi_panel.c: add powersuspend hooks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: cumulative update to version 1.5
    
    - fix hybrid-kernel mode cannot be set through sysfs
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.4 (Yank555.lu)
    
    - add hybrid-kernel mode (autosleep and panel, first wins)
    - default to hybrid-kernel mode
    - harmonize debug message with my other stuff
    - include all 3 modes as default, so it's only commenting out to change
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.3 (Yank555.lu)
    
    - fix stupid typo
    - add an alternative hook for screen on / off detection in the display panel driver
    (sleeps ~0.1s sooner, wakes up ~1s later then autosleep)
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.2 (Yank555.lu)
    
    - make kernel / userspace mode switchable
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.1 (Yank555.lu)
    
    - make powersuspend not depend on a userspace initiator anymore, but use a hook in autosleep instead.
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend (faux123)
    
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a replacement
    for it and existing early_suspend drivers can be easily adapted to use this
    new replacement driver
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    drivers/video/msm/mdss/mdss_dsi_panel.c: update powersuspend hook calls
    
    fix typo!
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Haseo97 <jawgattus@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -36,6 +36,11 @@
 
 #define MDSS_PANEL_DEFAULT_VER 0xffffffffffffffff
 #define MDSS_PANEL_UNKNOWN_NAME "unknown"
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 #define DCS_CMD_GET_POWER_MODE 0x0A
@@ -1207,6 +1212,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1372,6 +1381,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dba_utils_video_off(pinfo->dba_data);
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, false);
 	}
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 end:
 	pr_debug("%s:-\n", __func__);
