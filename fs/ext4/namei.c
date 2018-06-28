2167e2039a1a8aa097b414cf0518d1ea5b8ccb8f
yuedl1 <yuedl1@lenovo.com>
2018-06-28 20:08:14 +0800

    add log for debug encryption info mismatch
    add some log for debug encryption info
    
    Cherry-picked from (CR)
    
    Change-Id: I03ae2e27a3ec205d7225ea1e9d5655f25089ded7
    Reviewed-on: https://gerrit.mot.com/1257334
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Dongliang Yue <yuedl1@motorola.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1563,7 +1563,7 @@ static struct dentry *ext4_lookup(struct inode *dir, struct dentry *dentry, unsi
 	struct ext4_dir_entry_2 *de;
 	struct buffer_head *bh;
 
-       if (ext4_encrypted_inode(dir)) {
+	if (ext4_encrypted_inode(dir)) {
                int res = ext4_get_encryption_info(dir);
 
 		/*
@@ -1614,9 +1614,11 @@ static struct dentry *ext4_lookup(struct inode *dir, struct dentry *dentry, unsi
 								  inode)) {
 			iput(inode);
 			ext4_warning(inode->i_sb,
-				     "Inconsistent encryption contexts: %lu/%lu\n",
+				     "Inconsistent encryption contexts: %lu/%lu %s\n",
 				     (unsigned long) dir->i_ino,
-				     (unsigned long) inode->i_ino);
+				     (unsigned long) inode->i_ino, dentry->d_name.name);
+			iput(inode);
+			WARN_ON("Inconsistent encryption contexts");
 			return ERR_PTR(-EPERM);
 		}
 	}
