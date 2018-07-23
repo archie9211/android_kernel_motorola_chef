b7bdac1ae442b3854fdce883e5e018ee5f18fae5
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2018-07-23 17:15:18 -0500

    ASoC: wm_adsp: Limit I2C transfer buffer size
    I2C driver on Qcom platform only allows 15 256b TX/RX buffers
    per transmission. Limit max transfer size to 15*256 = 3840b.
    
    Change-Id: I74fc25f845597a1677134b66a36dce8e73d7ac18
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    
    Mot-CRs-Fixed:(CR)
    Change-Id: I74fc25f845597a1677134b66a36dce8e73d7ac18
    Reviewed-on: https://gerrit.mot.com/1215150
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    SLTApproved: Ravikumar Vembu <raviv@motorola.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -49,6 +49,12 @@
 #define adsp_dbg(_dsp, fmt, ...) \
 	dev_dbg(_dsp->dev, "DSP%d: " fmt, _dsp->num, ##__VA_ARGS__)
 
+/*
+ * Max I2C transfer size on Qcom platform, 3840 bytes (15 buffers * 256b)
+ */
+
+#define MAX_I2C_TX_SIZE                   0xf00
+
 #define ADSP1_CONTROL_1                   0x00
 #define ADSP1_CONTROL_2                   0x02
 #define ADSP1_CONTROL_3                   0x03
@@ -1340,8 +1346,8 @@ static int wm_coeff_read_control(struct wm_coeff_ctl *ctl,
 	memcpy(buf, scratch, ctl->len);
 	kfree(scratch);
 	while ((len - read_len) > 0) {
-		toread_len = (len - read_len) > PAGE_SIZE ?
-			PAGE_SIZE : (len - read_len);
+		toread_len = (len - read_len) > MAX_I2C_TX_SIZE ?
+			MAX_I2C_TX_SIZE : (len - read_len);
 
 		scratch = wm_adsp_buf_alloc(NULL, toread_len, &buf_list);
 		if (!scratch) {
@@ -1934,7 +1940,7 @@ static int wm_adsp_write_blocks(struct wm_adsp *dsp, const u8 *data, size_t len,
 				size_t burst_multiple)
 
 {
-	size_t to_write = PAGE_SIZE - (PAGE_SIZE % burst_multiple);
+	size_t to_write = MAX_I2C_TX_SIZE - (MAX_I2C_TX_SIZE % burst_multiple);
 	size_t remain = len;
 	struct wm_adsp_buf *buf;
 	unsigned int addr_div;
